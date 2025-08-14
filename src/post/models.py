bc28e443236a509ca3cb762f775477c1690e2c6c
Amadou6124 <148112454+Amadou6124@users.noreply.github.com>
2025-08-14 12:17:31 +0000

    Ajout du fichier image dans model Article et configuration de media files

--- src/post/models.py
+++ src/post/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(
+        max_length=300,  
+        help_text="Titre de l'article"
+    )
+    content = models.TextField(help_text="Contenu de l'article")
+    image = models.ImageField(
+        upload_to='images/',  
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)      
+
+    def __str__(self):
+        return self.title
