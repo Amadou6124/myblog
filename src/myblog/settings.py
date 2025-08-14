e503f0746ff5b36e403cea7c4fb6970b37a101a8
Amadou6124 <148112454+Amadou6124@users.noreply.github.com>
2025-08-14 09:56:39 +0000

    Configuration du projet django pour utiliser PostgreSQL

--- src/myblog/settings.py
+++ src/myblog/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myblog_db',
+        'USER': 'myblog_de_amadousy',
+        'PASSWORD': 'Alhamdoulilah100%',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
