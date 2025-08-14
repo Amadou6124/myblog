bcc1ba1e5b27d5be36756f18755918abbb1c845e
Amadou6124 <148112454+Amadou6124@users.noreply.github.com>
2025-08-14 12:56:35 +0000

    Ajout de la vue ArticleCreateView, template d'ajout d'article et mise à jour des urls

--- src/post/urls.py
+++ src/post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, ArticleCreateView
 
 
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('add/', ArticleCreateView.as_view(), name="article-add"),
 ]
--- src/post/views.py
+++ src/post/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+from .models import Article
+ 
+
 
 # Create your views here.
 def home(request):
     return render(request, 'post/index.html')
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ['title', 'content', 'image']
+    template_name = 'post/article_create.html'
+    success_url = reverse_lazy('home')
